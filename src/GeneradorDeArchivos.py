12294b730b1680b0f923a0e8f5cf1854b8064e93
Author Name <nataniel.cruz@hotmail.com>
2024-05-27 11:32:39 -0300

    Eliminada la carpeta de .vscode y corregido bug en GeneradorDeArchivos

--- src/GeneradorDeArchivos.py
+++ src/GeneradorDeArchivos.py
@@ -22,31 +22,30 @@ class GeneradorDeArchivos:
         
         celdaIni = 0
 
-        for fila in ws.iter_rows(max_col = 30):
+        for fila in ws.iter_rows(max_col = COLUM_MAX + 1):
             for celda in fila:
                 if(celda.fill.fgColor.rgb == self.colorIncial and celdaIni == 0):
                     celdaIni = celda
-                    fp = open(self.rutaArchivos + '\\' + celdaIni.value + ".csv", "wt")
-                    fp.write(header + '\n')
-                    
-                #Tengo que revisar que onda como detectar la celda Y*ROW* sea mayor a 0, falta eso
 
-            
-                    
-                if(celda.column_letter == 'Y' and celda.fill.fgColor.rgb == self.colorFinal and celda.value < 0 ):
-                    i = celdaIni.row
-                    print(ws['Y'+ str(celda.row)])
-                    while i < celda.row:
-                        reemplazar = ['None', '[', ']', '\'', '\"']
-                        data = str([ws.cell(row=i, column = i).value for i in range(1, COLUM_MAX + 1)])
 
-                        for caracter in reemplazar:
-                            data = data.replace(caracter, '')
+ 
+                if(celda.column_letter == 'Y' and celda.fill.fgColor.rgb == self.colorFinal):
+                    if(celda.value < 0):
+                      i = celdaIni.row
+                      fp = open(self.rutaArchivos + '\\' + celdaIni.value + ".csv", "wt")
+                      fp.write(header + '\n')
 
-                        data = data.replace(',', ';')
-                        fp.write(data + '\n')
-                        i += 1
+                      print(ws['Y'+ str(celda.row)])
+                      while i < celda.row + 1:
+                          reemplazar = ['None', '[', ']', '\'', '\"']
+                          data = str([ws.cell(row=i, column = j).value for j in range(1, COLUM_MAX + 1)])
+ 
+                          for caracter in reemplazar:
+                              data = data.replace(caracter, '')
 
+                          data = data.replace(',', ';')
+                          fp.write(data + '\n')
+                          i += 1
                     celdaIni = 0
                     fp.close()
 
