35340d2845576984e88adc82117603d3574f9bab
Nataniel Cinquegrani <nataniel.cinquegrani@hzgroup.com.ar>
2024-05-31 11:42:06 -0300

    Conversor actualizado y terminado. Importadas funciones de os para listar y borrar archivos

--- src/Conversor.py
+++ src/Conversor.py
@@ -1,24 +1,24 @@
 from openpyxl import Workbook
-from openpyxl.styles import Font, Border, Side, PatternFill, Protection
+from openpyxl.styles import Font, Border, Side, PatternFill
 import csv
 
-class Conversor:
-    def __init__(self, rutaArchivo, rutaConvertido):
-        self.rutaArchivo = rutaArchivo
-        self.rutaConvertido = rutaConvertido
-        
-    def convertir_csv_xlsx(self):
+class Conversor:    
+    def convertir_csv_xlsx(rutaArchivo, rutaConvertido):
         wb = Workbook()
         ws = wb.active
+        anchoCol = 14.5
 
-        with open(self.rutaArchivo, 'rt') as fp:
+        with open(rutaArchivo, 'rt') as fp:
             for row in csv.reader(fp, delimiter=';'):
                 ws.append(row)
-
+    
         for rows in ws.iter_rows(min_row=ws.max_row, max_row=ws.max_row, min_col=1):
             for cell in rows:
                 cell.font = Font(bold=True)
                 cell.fill = PatternFill(start_color="00c1f5", end_color="00c1f5", fill_type="solid")
                 cell.border = Border(top=Side(style='medium'), bottom=Side(style='medium'))
-        
-        wb.save(self.rutaConvertido)
+                
+        for col in ws.iter_cols():
+            ws.column_dimensions[col[0].column_letter].width = anchoCol
+
+        wb.save(rutaConvertido)
